368d61c972194331f88e630a7144b44e6cde8e29
karthik0037 <70190511+karthik0037@users.noreply.github.com>
2020-10-04 16:11:50 +0530

    Add files via upload

---
+++ linked list preprend.py
@@ -0,0 +1,43 @@
+print("karthik-121910313037")
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def append(self,value):
+        if self.head is None:
+            new_node  = Node(value)
+            self.head=new_node
+        else:
+            new_node = Node(value)
+            temp=self.head
+            while temp.next is not None:
+                temp=temp.next
+            temp.next=new_node    
+
+    def prepend(self,value):
+        if self.head is None:
+            new_node  = Node(value)
+            self.head=new_node
+        else:
+            new_node = Node(value)
+            new_node.next=self.head
+            self.head=new_node
+        
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+d=LinkedList()
+N=int(input("enter the number of nodes u want to add to the linkedlist: "))
+for i in range(N):
+    d.append(input(f'Enter the node data'))
+a=input("Enter the value to add at the beggining: ")
+d.prepend(a)
+d.printList() 
